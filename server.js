26012ba56b75dd89c56841ce0aeb04ef375c9718
gracehalbert <gracehalbert@gmail.com>
2016-12-19 16:57:17 -0600

    env file added

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-mongoose.connect('mongodb://localhost/whats-in-my-pantry');
+var app = express();
+var port = process.env.PORT;
+var uri = process.env.URI;
 
-app.use(express.static(__dirname + '/client'));
+mongoose.connect(uri);
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-app.get('/', function(req, res) {
-  res.send("Hello World");
+var Item = mongoose.model('Item', {
+  text: String
 });
 
-app.listen(process.env.PORT || 8080);
-console.log('App listening on port 8080');
+// app.get('/api/items', function(req, res) {
+//   Item.find(function(err, items) {
+//     if (err) { res.send(err); }
+//     res.json(items);
+//   })
+// });
+//
+// app.post('/api/items', function(req, res) {
+//   Item.create({
+//     text: req.body.text,
+//     done: false
+//   }, function(err, item) {
+//     if (err) { res.send(err); }
+//     Item.find(function(err, items) {
+//       if (err) { res.send(err); }
+//       res.json(items);
+//     });
+//   });
+// });
+//
+// app.delete('/api/todos/:todo_id', function(req, res) {
+//   Item.remove({
+//
+//   })
+// })
+
+app.listen(port || 8080);
+console.log('App listening ' + port);
